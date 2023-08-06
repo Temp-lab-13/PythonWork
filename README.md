d7362381fe163bbff7a269a43c71cad0546db3f1
Temp-lab-13 <tap-kro-tap@protonmail.com>
2023-08-06 17:13:34 +0700

    Буткемп, 1-я работа с Flask

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # PythonWork
+.Work\Scripts\activate - команда для активации виртуальной среды.
+
+pip install Flask - установка Flask
+
+pip freeze - посмотреть что установилось
+
+flask --app Work.py run - принудительный запуск
--- Work.py
+++ Work.py
@@ -1,1 +1,21 @@
-print(1)
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hello world'
+
+
+@app.route('/<name>/')
+def func_name(name):
+    print(type(name))
+    return f'<h1>Привет</h1>, <p>{name}</p>'
+
+@app.route('/temp/')
+def temp():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run()
+
